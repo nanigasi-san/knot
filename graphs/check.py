6488c28910c239764dbe95776b532e08bd08f763
nanigasi_san <nanigasi.py@gmail.com>
2021-01-11 01:02:15 +0900

    assert するようにした

--- graphs/check.py
+++ graphs/check.py
@@ -5,4 +5,4 @@ filenames = ["0", "1", "2_1", "2_2", "2_3"]
 
 for fn in filenames:
     g, ec = convert.kng_to_graph_and_ec(f"./data/{fn}.kng")
-    print(fn, is_same_circuit(ec, get_eulerian_circuit(g)))
+    assert is_same_circuit(ec, get_eulerian_circuit(g))
