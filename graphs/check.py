1c9cc62cd92983d8e30400f29f6598b51a9875f2
nanigasi_san <nanigasi.py@gmail.com>
2021-01-11 02:05:20 +0900

    data以下の.kngを全てチェックするようにした

--- graphs/check.py
+++ graphs/check.py
@@ -1,8 +1,11 @@
 import convert
 from algorithms import get_eulerian_circuit, is_same_circuit
+from glob import glob
 
-filenames = ["0", "1", "2_1", "2_2", "2_3"]
+filenames = glob("data/*.kng")
+
+print(filenames)
 
 for fn in filenames:
-    g, ec = convert.kng_to_graph_and_ec(f"./data/{fn}.kng")
-    assert is_same_circuit(ec, get_eulerian_circuit(g))
+    g, ec = convert.kng_to_graph_and_ec(fn)
+    print(fn, is_same_circuit(ec, get_eulerian_circuit(g)))
