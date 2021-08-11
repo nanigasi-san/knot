0a84c3a90a310c59d261a18581e49d5461257d19
nanigasi_san <nanigasi.py@gmail.com>
2021-08-11 23:28:18 +0900

    AltasViewから辺のリストへの変換

--- graphs/algorithms.py
+++ graphs/algorithms.py
@@ -189,11 +189,11 @@ def integration_nodes(g):
         c1 = len(g[p1][p2]) if p2 in g.succ[p1] else 0
         c2 = len(g[p2][p1]) if p1 in g.succ[p2] else 0  # TODO: A/Bに触れてない
 
-        def can_integ_type_a(a, b):
+        def can_integ_type_a(a, b) -> bool:
             data = tuple(dict(g[a][b]).values())
             return data[0]["Tu"] != data[1]["Tu"] and data[0]["Tv"] != data[1]["Tv"]
 
-        def can_integ_type_b(a, b):
+        def can_integ_type_b(a, b) -> bool:
             data1 = tuple(dict(g[a][b]).values())[0]
             data2 = tuple(dict(g[b][a]).values())[0]
             return data1["Tu"] != data2["Tv"] and data1["Tv"] != data2["Tu"]
@@ -212,14 +212,32 @@ def integration_nodes(g):
             for p2, parity2 in g.nodes(data="parity"):
                 conn, conn_type = is_connect(p1, p2)
                 if conn:
-                    if conn_type == 1:
+                    if conn_type == 1:  # a-1
                         print(p1, p2)
-                    # p1, p2が含まれていない二辺を探し、a, b, c, dとTa...Tdを振る。この時T1a...T2bも振る
-                        for_p1 = g.pred[p1].items()
-                        from_p2 = g.succ[p2].items()
-                        print(for_p1)
-                        print(from_p2)
-                    elif conn_type == 2:  # a
+                        # p1, p2が含まれていない二辺を探し、a, b, c, dとTa...Tdを振る。この時T1a...T2bも振る
+
+                        def pred_to_tuple(edges, p):
+                            ans = []
+                            for k in edges.keys():
+                                for v in edges[k].values():
+                                    e = (k, p, v["Tu"], v["Tv"])
+                                    ans.append(e)
+                            return ans
+
+                        def succ_to_tuple(edges, p):
+                            ans = []
+                            for k in edges.keys():
+                                for v in edges[k].values():
+                                    e = (p, k, v["Tu"], v["Tv"])
+                                    ans.append(e)
+                            return ans
+
+                        for_p1 = g.pred[p1]
+                        from_p2 = g.succ[p2]
+                        print(pred_to_tuple(for_p1, p1))
+                        print(succ_to_tuple(from_p2, p2))
+
+                    elif conn_type == 2:  # a-2
                         pass
                     else:  # b
                         pass
--- graphs/sample2.py
+++ graphs/sample2.py
@@ -5,4 +5,4 @@ from useful import convert_edges_for_list_of_tuple, print_graph
 import networkx as nx
 g = keg_to_graph("data/integration/1_1.keg")
 x = integration_nodes(g)
-# print_graph(x)
+print_graph(x)
