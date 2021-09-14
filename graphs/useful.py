4a48ea24185558e7b66f45ecdb894a2c32e09f09
nanigasi_san <nanigasi.py@gmail.com>
2021-09-14 19:58:49 +0900

    lint

--- graphs/useful.py
+++ graphs/useful.py
@@ -1,5 +1,4 @@
 import networkx as nx
-from copy import deepcopy
 
 A = "A"
 B = "B"
@@ -7,15 +6,19 @@ N = None
 Odd = "Odd"
 Even = "Even"
 
+
 def add_node(g: nx.MultiDiGraph, node: int, parity) -> None:
     g.add_node(node, parity=parity)
+
 
 def add_edge(g: nx.MultiDiGraph, u: int, v: int, Tu, Tv) -> None:
     g.add_edge(u, v, Tu=Tu, Tv=Tv)
 
+
 def print_graph(g: nx.MultiDiGraph) -> None:
     print("Nodes:", [(node, g.nodes[node]["parity"]) for node in g.nodes])
     print("Edges:", [(edge[0], edge[1], *list(g.get_edge_data(*edge[:2])[0].values())) for edge in g.edges])
+
 
 def convert_edges_for_list_of_tuple(edges):
     ans = []
