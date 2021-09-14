ac3565d72223392ccaad12d8ea63c2a935786a42
nanigasi_san <nanigasi.py@gmail.com>
2021-09-14 19:50:54 +0900

    lint

--- graphs/sample.py
+++ graphs/sample.py
@@ -1,8 +1,7 @@
-from algorithms import rebuild, get_eulerian_circuit
+from algorithms import rebuild
 from convert import keg_to_graph
 from useful import convert_edges_for_list_of_tuple
 
-import networkx as nx
 g1 = keg_to_graph("data/rebuild/2_1.keg")
 x1 = rebuild(g1)
 print("=====")
@@ -12,9 +11,8 @@ x2 = rebuild(g2)
 i = x1[0]
 for j in x2:
     nodes_eq = (sorted(i.nodes(data=True)) == sorted(j.nodes(data=True)))
-    edges_eq = (sorted(convert_edges_for_list_of_tuple(i.edges(data=True))) == sorted(convert_edges_for_list_of_tuple(j.edges(data=True)))) 
+    edges_eq = (sorted(convert_edges_for_list_of_tuple(i.edges(data=True))) == sorted(convert_edges_for_list_of_tuple(j.edges(data=True))))
     if nodes_eq and edges_eq:
-        return True
+        print("Yes")
     else:
-        return False
-
+        print("No")
